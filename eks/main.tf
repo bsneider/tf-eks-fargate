7b31a8399cfa383ebbea51288f024f03bffa9380
Brandon Sneider <54788551+bsneider@users.noreply.github.com>
2021-07-30 09:34:21 -0500

    [TerraformCICD] update to latest providers and use newer syntax

--- eks/main.tf
+++ eks/main.tf
@@ -1,13 +1,31 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.1.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+    external = {
+      source = "hashicorp/external"
+      version = "2.1.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.3.2"
+    }
+  }
+}
+
 provider "local" {
-  version = "~> 1.4"
 }
 
 provider "template" {
-  version = "~> 2.1"
 }
 
 provider "external" {
-  version = "~> 1.2"
 }
 
 provider "kubernetes" {
@@ -15,7 +33,6 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 1.10"
 }
 
 data "aws_eks_cluster" "cluster" {
