337bf99f42c24bd0084a3389219780d81f0d3298
Brandon Sneider <54788551+bsneider@users.noreply.github.com>
2021-07-30 09:41:21 -0500

    [TerraformCICD] remove depricated config https://registry.terraform.io/providers/hashicorp/kubernetes/1.11.0/docs May need to add back in some regard later

--- eks/main.tf
+++ eks/main.tf
@@ -19,20 +19,10 @@ terraform {
   }
 }
 
-provider "local" {
-}
-
-provider "template" {
-}
-
-provider "external" {
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
 }
 
 data "aws_eks_cluster" "cluster" {
